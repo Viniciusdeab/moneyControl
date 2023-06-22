f4f78edaf42fa2131a6e66f4a9ddcca5845d1393
Vinícius Brito <viniciusdeab@outlook.com>
2023-06-21 23:00:02 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello world, i'm Vinícius</h1>
       <GlobalStyle />
     </div>
   );
